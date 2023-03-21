850f9a149db151e9c13515d154db0e226e003350
HRISHIKESH KUMAR <hri123@gmail.com>
2023-03-21 21:40:27 +0530

    NA

--- tf-idf-vectorizer.py
+++ tf-idf-vectorizer.py
@@ -34,6 +34,24 @@ sentences = ['The cat sat on the mat',
              'The cat sat on the rug',
              'The mat was where the cat sat']
 
+
+import spacy
+nlp = spacy.load('en_core_web_lg')
+
+
+file = '/Users/hrishikesh/H/GoogleDrive/OPO/Obsidian/DayJob/Projects/Lakehouse/Lakehouse-Root.md'
+#open text file in read mode
+text_file = open(file, "r")
+#read whole file to a string
+text = text_file.read()
+#close file
+text_file.close()
+
+
+emma = nlp(text).sents
+# sentences = ["".join(str(list_of_words)) for list_of_words in emma]
+
+
 # Preprocess sentences
 processed_sentences = [preprocess(sentence) for sentence in sentences]
 
@@ -45,7 +63,7 @@ tfidf_vectors = vectorizer.fit_transform(processed_sentences)
 cosine_similarities = cosine_similarity(tfidf_vectors)
 
 # Find duplicate sentences based on cosine similarity threshold
-duplicate_threshold = 0.3
+duplicate_threshold = 1
 for i in range(len(sentences)):
     for j in range(i+1, len(sentences)):
         if cosine_similarities[i][j] >= duplicate_threshold:
@@ -62,5 +80,10 @@ def get_tf_idf_query_similarity(documents, query):
     cosineSimilarities = cosine_similarity(queryTFIDF, docTFIDF)
     return cosineSimilarities
 
-print(get_tf_idf_query_similarity(sentences, 'The cat sat on the mat'))
+cosineSimilarities = get_tf_idf_query_similarity(sentences, 'cluster access')
+cosineSimilarities = get_tf_idf_query_similarity(sentences, 'The cat sat on the mat')
+
+for i in range(len(sentences)):
+    print("Score: ", cosineSimilarities[0][i], "for sentence: ", sentences[i])
+
 
--- using-spacy.py
+++ using-spacy.py
@@ -7,20 +7,36 @@ import spacy
 nlp = spacy.load('en_core_web_lg')
 
 
+file = '/Users/hrishikesh/H/GoogleDrive/OPO/Obsidian/DayJob/Projects/Lakehouse/Lakehouse-Root.md'
+#open text file in read mode
+text_file = open(file, "r")
+#read whole file to a string
+text = text_file.read()
+#close file
+text_file.close()
+
+
+emma = nlp(text).sents
+sentences = ["".join(str(list_of_words)) for list_of_words in emma]
+
+
 # List of sentences to compare
-sentences = ['The cat sat on the mat', 
-             'Obama speaks to the media in Illinois', 
-             'The President greets the press in Chicago', 
-             'The dog sat on the mat', 
-             'The cat in the hat sat on the mat', 
-             'The cat sat on the rug',
-             'The mat was where the cat sat']
+# sentences = ['The cat sat on the mat', 
+#              'Obama speaks to the media in Illinois', 
+#              'The President greets the press in Chicago', 
+#              'The dog sat on the mat', 
+#              'The cat in the hat sat on the mat', 
+#              'The cat sat on the rug',
+#              'The mat was where the cat sat']
 
-query = 'The cat sat on the mat'
-query = 'Obama speaks to the media in Illinois'
+# query = 'The cat sat on the mat'
+# query = 'Obama speaks to the media in Illinois'
 
+query = "cluster access"
 query_processed = nlp(query)
 
 for i in range(len(sentences)):
     sentence_processed = nlp(sentences[i])
     print("Similarity: ", query_processed.similarity(sentence_processed), ' ', sentences[i])
+    print('-' * 20)
+
