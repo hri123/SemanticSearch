0184c6791573c3b02362b81c1f550a19e27bb1f2
HRISHIKESH KUMAR <hri123@gmail.com>
2023-03-24 18:15:49 +0530

    NA

--- using-spacy.py
+++ using-spacy.py
@@ -42,3 +42,79 @@ for i in range(len(sentences)):
 
 
 # TODO: https://github.com/gandersen101/spaczz
+
+import spacy
+from spaczz.matcher import FuzzyMatcher
+
+nlp = spacy.blank("en")
+text = """Grint Anderson created spaczz in his home at 555 Fake St,
+Apt 5 in Nashv1le, TN 55555-1234 in the US."""  # Spelling errors intentional.
+doc = nlp(text)
+
+matcher = FuzzyMatcher(nlp.vocab)
+matcher.add("NAME", [nlp("Grant Andersen")])
+matcher.add("GPE", [nlp("Nashville")])
+matches = matcher(doc)
+
+for match_id, start, end, ratio in matches:
+    print(match_id, doc[start:end], ratio)
+
+import spacy
+from spacy.tokens import Span
+from spaczz.matcher import FuzzyMatcher
+
+nlp = spacy.blank("en")
+text = """Grint Anderson created spaczz in his home at 555 Fake St,
+Apt 5 in Nashv1le, TN 55555-1234 in the US."""  # Spelling errors intentional.
+doc = nlp(text)
+
+
+def add_name_ent(matcher, doc, i, matches):
+    """Callback on match function. Adds "NAME" entities to doc."""
+    # Get the current match and create tuple of entity label, start and end.
+    # Append entity to the doc's entity. (Don't overwrite doc.ents!)
+    _match_id, start, end, _ratio = matches[i]
+    entity = Span(doc, start, end, label="NAME")
+    doc.ents += (entity,)
+
+
+matcher = FuzzyMatcher(nlp.vocab)
+matcher.add("NAME", [nlp("Grant Andersen")], on_match=add_name_ent)
+matches = matcher(doc)
+
+for ent in doc.ents:
+    print((ent.text, ent.start, ent.end, ent.label_))
+
+import spacy
+from spaczz.matcher import FuzzyMatcher
+
+nlp = spacy.blank("en")
+# Let's modify the order of the name in the text.
+text = """Anderson, Grint created spaczz in his home at 555 Fake St,
+Apt 5 in Nashv1le, TN 55555-1234 in the US."""  # Spelling errors intentional.
+doc = nlp(text)
+
+matcher = FuzzyMatcher(nlp.vocab)
+matcher.add("NAME", [nlp("Grant Andersen")])
+matches = matcher(doc)
+
+# The default fuzzy matching settings will not find a match.
+for match_id, start, end, ratio in matches:
+    print(match_id, doc[start:end], ratio)
+
+import spacy
+from spaczz.matcher import FuzzyMatcher
+
+nlp = spacy.blank("en")
+# Let's modify the order of the name in the text.
+text = """Anderson, Grint created spaczz in his home at 555 Fake St,
+Apt 5 in Nashv1le, TN 55555-1234 in the US."""  # Spelling errors intentional.
+doc = nlp(text)
+
+matcher = FuzzyMatcher(nlp.vocab)
+matcher.add("NAME", [nlp("Grant Andersen")], kwargs=[{"fuzzy_func": "token_sort"}])
+matches = matcher(doc)
+
+# The default fuzzy matching settings will not find a match.
+for match_id, start, end, ratio in matches:
+    print(match_id, doc[start:end], ratio)
